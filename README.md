b07ad2a583331df605a61181bfd9f8ca6eb1f5ba
Vondreii <Sharlene.VonDrehnen@outlook.com>
2023-02-18 21:30:20 +1100

    Added enums example

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # CS-enum-examples
-These include examples on how to use enums in C#
+
+These include examples on how to use enums in C#.
+
+Things related to enums include:
+
+- How to declare an enum
+- Different ways to access enum values
+- Comparing enums
+- Iterating over enums (using getNames() and getValues())
+
+## Prerequisites
+
+- Make sure you have Visual Studio installed.
+
+## To Open
+
+- Navigate to the **enum-example** folder.
+- Click on the **enum-example.sln** file to open the solution.
+
+## To Run
+
+- Click on the green play button at the top of the Visual Studio solution. This will open a console that prints the values shown in the examples.
---
+++ enum-example/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace enum_example
+{
+    class Program
+    {
+        enum Weekday
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            // Access enum #1
+            string friday = Enum.GetName(typeof(Weekday), Weekday.Friday);
+            Console.WriteLine("Thank God it's " + friday + "!"); // Output = Thank God it's Friday!
+
+            // Access enum #2
+            Weekday tuesday = (Weekday)2;
+            Console.WriteLine("Today is " + tuesday); // Output = Today is Tuesday
+
+            // Access enum #3
+            Console.WriteLine(Weekday.Thursday + " is the new " + Weekday.Friday); // Output = Thursday is the new Friday
+
+            // Access enum #4
+            int mondayIndex = (int)Weekday.Monday;
+            Console.WriteLine(mondayIndex); // Output = 1
+
+            // Compare enum #1
+            Console.WriteLine("");
+            Weekday day1 = Weekday.Monday;
+            Weekday day2 = Weekday.Tuesday;
+
+            if (day1 == day2)
+            {
+                Console.WriteLine("The days are the same.");
+            }
+            else
+            {
+                Console.WriteLine("The days are different.");
+            }
+
+            // Compare enum #2
+            if (day1.Equals(day2))
+            {
+                Console.WriteLine("The days are the same.");
+            }
+            else
+            {
+                Console.WriteLine("The days are different.");
+            }
+
+            // Compare enum #3
+            Weekday valueToCompare = Weekday.Wednesday;
+            switch (valueToCompare)
+            {
+                case Weekday.Monday:
+                    Console.WriteLine("I need coffee.");
+                    break;
+                case Weekday.Tuesday:
+                    Console.WriteLine("I need coffee.");
+                    break;
+                case Weekday.Wednesday:
+                    Console.WriteLine("I need coffee.");
+                    break;
+                case Weekday.Thursday:
+                    Console.WriteLine("Thursday is the new Friday!");
+                    break;
+                case Weekday.Friday:
+                    Console.WriteLine("Thank God it's Friday!");
+                    break;
+                case Weekday.Saturday:
+                    Console.WriteLine("My favorite day!");
+                    break;
+                case Weekday.Sunday:
+                    Console.WriteLine("My other favorite day!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day of the week.");
+                    break;
+            }
+
+            // Iterate over enums #1
+            Console.WriteLine("");
+            foreach (Weekday day in Enum.GetValues(typeof(Weekday)))
+            {
+                //Console.WriteLine((int)day); // To print the index
+                Console.WriteLine(day); // To print the text value
+            }
+
+            // Iterate over enums #2
+            Console.WriteLine("");
+            foreach (string name in Enum.GetNames(typeof(Weekday)))
+            {
+                //int index = (int)(Weekday)Enum.Parse(typeof(Weekday), name);
+                //Console.WriteLine(index); // To print the index
+
+                Console.WriteLine(name); // To print the text value
+            }
+        }
+    }
+}
